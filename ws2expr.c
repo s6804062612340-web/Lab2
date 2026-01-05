a40a4b1937cc1b8259da0cbf56ce48582c49c93e
Thanwat <s6804062612340@email.kmutnb.ac.th>
2026-01-05 22:03:56 +0700

    Create ws2expr.c

---
+++ ws2expr.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    printf("4.1\n");
+    float a=1,b=1,c=5,d=1;
+    printf("WS2A: %f\n",(3.0*(a/5.0)+(1.0/b)));
+    printf("WS2B: %f\n",((3.0*a)+(5.0*b))/(2.0+c));
+    printf("WS2C: %f\n",(2.0/7.0)*(pow(4.0,(3+c))-(5.0*d)));
+    printf("WS2D: %f\n",sqrt((2.0+(8.0*b))/a));
+    printf("WS2F: %f\n",cbrt(pow(b,2.0)-(4.0*d)));
+
+    printf("4.2\n");
+    float a2=9,b2=2,c2=0,d2=1;
+    printf("WS2A: %f\n",(3.0*(a2/5.0)+(1.0/b2)));
+    printf("WS2B: %f\n",((3.0*a2)+(5.0*b2))/(2.0+c2));
+    printf("WS2C: %f\n",(2.0/7.0)*(pow(4.0,(3+c2))-(5.0*d2)));
+    printf("WS2D: %f\n",sqrt((2.0+(8.0*b2))/a2));
+    printf("WS2F: %f\n",cbrt(pow(b2,2.0)-(4.0*d2)));
+
+        printf("4.3\n");
+    float a3=0,b3=3,c3=3,d3=3;
+    printf("WS2A: %f\n",(3.0*(a3/5.0)+(1.0/b3)));
+    printf("WS2B: %f\n",((3.0*a3)+(5.0*b3))/(2.0+c3));
+    printf("WS2C: %f\n",(2.0/7.0)*(pow(4.0,(3+c3))-(5.0*d3)));
+    printf("WS2D: %f\n",sqrt((2.0+(8.0*b3))/a3));
+    printf("WS2F: %f\n",cbrt(pow(b3,2.0)-(4.0*d3)));
+}
